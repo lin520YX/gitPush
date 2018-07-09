03b9545032c8ea8549bf28797c45c580adbcc76f
bruce-zxy <bruce_zxy@163.com>
2018-07-09 23:55:38 +0800

    create Singer page content

---
+++ white-App/src/components/Singer/Singer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="singer">
+    <div class="menu">视频学习<span></span></div>
+    <div class="menu">期初建账<span></span></div>
+    <div class="menu">登录做账<span></span></div>
+    <div class="menu">查账<span></span></div>
+    <div class="menu">寻找后台协助<span></span></div>
+  </div>
+</template>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+  #singer
+    display:flex
+    flex-direction:column
+    justify-content:center
+    align-item:center
+
+</style>
--- white-App/src/router/index.js
+++ white-App/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Singer from '../components/Singer/Singer';
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
-  ]
+  routes: [{
+    	path: '/singer',
+    	name: 'singer',
+    	component: Singer
+    }]
 })
