0ed75f0aa8989742dd3eff79e5df3fe0c9404796
lin13177896353@163.com <lin13177896353@163.com>
2018-07-09 23:40:35 +0800

    m-header

---
+++ white-App/src/components/m-header/m-header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="m-header">
+      {{title}}
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+      props:{
+          title:{
+            type:String,
+            default:'小白会做账'
+          }
+      }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .m-header
+    height: 44px
+    line-height :44px
+    color:#ff
+    font-weight:300
+    text-align: center
+    background : $public-color
+    font-size: 18px
+    overflow:hidden
+   
+</style>
